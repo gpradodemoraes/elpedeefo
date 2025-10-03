f6c7a3b3e95738be7db66de707897f7c950f8f41
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-10-03 14:01:13 -0300

    usando aritmetica de ponteiros para compilar em compiladores que não aceitam o *char[] sem tamanho conhecido em compilacao

--- test.c
+++ test.c
@@ -15,25 +15,28 @@ int main() {
         "-eol","unix"
     };
     int argumentssize = sizeof(argumentsbase) / sizeof(argumentsbase[0]);
-    char *arguments[argumentssize + 1];
+    char **arguments = malloc(sizeof(argumentsbase[0]) * (argumentssize + 1));
     char *elpedfpointer = NULL;
 
     arguments[6] = (char*)malloc(1024);
 
     for(int i = 0; i < argumentssize; i++) {
-    	arguments[i] = (char*) argumentsbase[i];
+    	*(arguments + i) = (char*) argumentsbase[i];
+        printf("%d => %s\n", i, *(arguments + i));
     }
 
     strcpy_s(arguments[6], 1024, "../teste_pdf.pdf");
+    printf("%d => %s\n", 6, *(arguments + 6));
 
     if((convertpdftotext(
-    sizeof(arguments) / sizeof(arguments[0]),
+    argumentssize + 1,
     (char**)arguments,&elpedfpointer) != 0)) {
         sprintf_s(errortext, 200, "erro: não foi possível converter o arquivo %s", arguments[6]);
         return 5;
     }
     printf("====================================\n%s\n====================================\n", elpedfpointer);
     free(arguments[6]);
+    free(arguments);
     free(elpedfpointer);
     return 0;
 }
